ba27f0f4bd1573504aa1c1ffc046e10f59846c37
vinod0111 <vinod.patil.engineer@gmail.com>
2023-08-16 00:39:03 +0530

    api gateway code push

---
+++ API-gateway/src/main/java/com/example/bmcgateway/APIGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.example.bmcgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class APIGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APIGatewayApplication.class, args);
+	}
+
+
+}
